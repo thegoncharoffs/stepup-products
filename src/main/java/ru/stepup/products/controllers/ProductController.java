c9647a5e4b9d640abbd7b4faf5643b6c9989e974
Гончаров Борис Алексеевич (4069533) <goncharovb@dev.vtb.ru>
2024-03-07 14:18:59 +0300

    Imports removed

--- src/main/java/ru/stepup/products/controllers/ProductController.java
+++ src/main/java/ru/stepup/products/controllers/ProductController.java
@@ -1,10 +1,8 @@
 package ru.stepup.products.controllers;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
